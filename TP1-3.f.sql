6cccf5373dcfae100a89009746a685be4811f435
lelinhle2002 <52713689+lelinhle2002@users.noreply.github.com>
2025-10-01 22:13:07 +0200

    r

---
+++ TP1-3.f.sql
@@ -0,0 +1,21 @@
+CREATE OR REPLACE FUNCTION GET_ENSEIGNANT 
+    ( P_NOM IN ENSEIGNANT.NOM%TYPE,
+    P_PRENOM IN ENSEIGNANT.PRENOM%TYPE) RETURN VARCHAR2 IS
+    e_code ENSEIGNANT.IDENSEIGN%TYPE;
+     BEGIN
+        BEGIN 
+            SELECT IDENSEIGN
+            INTO e_code
+            FROM ENSEIGNANT
+            WHERE ENSEIGNANT.NOM = P_NOM 
+            AND
+            (ENSEIGNANT.PRENOM = P_PRENOM OR P_PRENOM IS NULL);
+        EXCEPTION
+            WHEN NO_DATA_FOUND THEN
+                e_code := NULL;
+            WHEN TOO_MANY_ROWS THEN
+                RAISE_APPLICATION_ERROR(-20103,'Au moins deux enseignants s''appellent ' || P_PRENOM || ' ' || P_NOM);
+        END;
+        RETURN e_code;
+END;   
+/
